ed78acd8689ea9ac5493cc6e39bb84152104284d
klem95 <enxbox@hotmail.com>
2020-10-01 16:50:20 +0200

    Base model

--- BasicLstmModel.py
+++ BasicLstmModel.py
@@ -4,16 +4,18 @@ from keras.layers import LSTM, Dense, Flatten
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 import matplotlib.pyplot as plt
-from Parameters import Labeler
+from Parameters import Labeler, RecordSettings
 from tensorflow.keras.utils import to_categorical
 import pandas as pd
 import glob2
+import tensorflow as tf
+import csv
 
 
 class BasicLstmModel:
     first_layer_rollouts = 50
 
-    def __init__(self,batch_size, epochs):
+    def __init__(self, batch_size, epochs):
         self.batch_size = batch_size
         self.epochs = epochs
 
@@ -21,13 +23,32 @@ class BasicLstmModel:
         all_files = glob2.glob(path + "/*.csv")
 
         self.dfs = []
+        labels = []
         for filename in all_files:
-            self.dfs.append(pd.read_csv(filename))
+            #            self.dfs.append(np.array(pd.read_csv(filename)))
+            with open(filename, newline='') as csvfile:
+                firstLine = True
+                # spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+                spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+                sample = []
+                for row in spamreader:
+                    if firstLine:
+                        firstLine = False
+                        continue
+                    labels.append(filename[5:-7])
+                    float_list = [float(s.replace(',', '')) for s in row]
+                    self.dfs.append(np.asarray(float_list).astype(float))
+                    #sample.append(np.asarray(float_list).astype(float))
+                # print('------')
+                # self.dfs.append(np.asarray(sample))
+
         self.label_encoder = LabelEncoder()
         self.oneHot_encoder = OneHotEncoder(sparse=False)
-        self.onehotLabels = self.encode_labels(all_files)
+        self.onehotLabels = self.one_hot(labels);
 
-    def encode_labels(self,file_names):
+        #self.onehotLabels = self.encode_labels(all_files)
+
+    def encode_labels(self, file_names):
         mappedFileNames = []
         for filename in file_names:
             mappedFileNames.append(filename[5:-7])
@@ -36,14 +57,48 @@ class BasicLstmModel:
         onehot_encoded = self.oneHot_encoder.fit_transform(integer_encoded)
         return onehot_encoded
 
+    def one_hot(self, data):
+        integer_encoded = self.label_encoder.fit_transform(data)
+        integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+        onehot_encoded = self.oneHot_encoder.fit_transform(integer_encoded)
+        return onehot_encoded
+
 
     def train_model(self):
-        xTrain = shape(t)
-        yTrain = self.onehotLabels
+        xTrain = np.asarray(self.dfs)
+        xTrain = xTrain.reshape(xTrain.shape[0],1,xTrain.shape[1])
+        yTrain = np.asarray(self.onehotLabels).astype(float)
 
-        xTest = shape(t)
-        yTest = self.onehotLabels
+        xValidate = np.asarray(self.dfs)
+        xValidate = xValidate.reshape(xValidate.shape[0],1,xValidate.shape[1])
 
+        yValidate = np.asarray(self.onehotLabels).astype(float)
+
+        print(type(xTrain))
+       # print(type(xTrain[50]))
+      #  print(xTrain[0])
+
+       # print(type(xTrain[0][0]))
+        #print(type(xTrain[0][0][0]))
+
+      #  print(yTrain.shape)
+     #   print(xTrain.shape)
+      #  print(type(xTrain[50][0][0]))
+        #print(len(xTrain[0]))
+        #print(len(xTrain[0][0]))
+
+        # print(xTrain)
+
+        model = Sequential()
+        model.add(LSTM(32, return_sequences=True, recurrent_dropout=0.1, input_shape=(30, 289)))
+        model.add(Flatten())
+        model.add(Dense(3, activation='softmax'))  # Classification
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        model.summary()
+
+        history = model.fit(xTrain, yTrain, epochs=self.epochs, validation_data=(xValidate, yValidate),
+                            batch_size=self.batch_size, validation_split=0.1)
 
         print("Train")
 
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from BasicLstmModel import BasicLstmModel
 
 def main():
     print("Hello World!")
-    mymodel = BasicLstmModel(2,3)
+    mymodel = BasicLstmModel(60,60)
     mymodel.train_model()
 
 if __name__ == "__main__":
