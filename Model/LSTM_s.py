b2452f275147678a7fafd96e095e3fc47715c246
klem95 <enxbox@hotmail.com>
2020-10-01 20:31:07 +0200

    initial bug fixed

---
+++ Model/LSTM_s.py
@@ -0,0 +1,86 @@
+import numpy as np
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.layers import LSTM, Dense, Flatten, Embedding
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import glob2
+import csv
+import matplotlib.pyplot as plt
+
+
+class LSTM_s:
+
+    def __init__(self):
+        self.batch_size = 20
+        self.epochs = 100
+        self.learning_rate = 0.01
+        self.total_dataset = []
+        self.label_size = 0
+
+    def retrieve_data(self):
+        path = r'Data'  # use your path
+        all_files = glob2.glob(path + "/*.csv")
+
+        for filename in all_files:
+            #            self.dfs.append(np.array(pd.read_csv(filename)))
+            with open(filename, newline='') as csvfile:
+                firstLine = True
+                dataScanner = csv.reader(csvfile, delimiter=';', quotechar='|')
+                sample = []
+                for row in dataScanner:
+                    if firstLine:
+                        firstLine = False
+                        continue
+                    float_list = [float(s.replace(',', '')) for s in row]
+                    sample.append(np.asarray(float_list).astype(float))
+                self.total_dataset.append(np.asarray(sample[:53]))  # <--- 54 is a problem...
+
+        self.label_encoder = LabelEncoder()
+        self.oneHot_encoder = OneHotEncoder(sparse=False)
+        self.onehotLabels = self.encode_labels(all_files)
+
+    def date_evaluation(self):
+        for sample in self.total_dataset:
+            for row in sample:
+                for val in row:
+                    if type(val) is not np.float:
+                        print(type(val))
+
+    def encode_labels(self, file_names):
+        mappedFileNames = []
+        for filename in file_names:
+            mappedFileNames.append(filename[5:-7])
+        integer_encoded = self.label_encoder.fit_transform(mappedFileNames)
+        integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+        onehot_encoded = self.oneHot_encoder.fit_transform(integer_encoded)
+        return onehot_encoded
+
+    def train_model(self):
+        self.retrieve_data()
+        self.date_evaluation()
+
+        x_train = np.asarray(self.total_dataset)
+        y_train = np.asarray(self.onehotLabels)
+        self.label_size = len(y_train[0])
+        x_validation = np.asarray(self.total_dataset)
+        y_validation = np.asarray(self.onehotLabels)
+
+        model = Sequential()
+        model.add(LSTM(100, return_sequences=True, recurrent_dropout=0.3, input_shape=(None, 289)))
+        model.add(LSTM(32, recurrent_dropout=0.2, activation='relu'))
+        model.add(Flatten())
+        model.add(Dense(self.label_size, activation='softmax'))  # Classification
+        model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=self.learning_rate),
+                      metrics=['accuracy'])
+
+        model.summary()
+
+        history = model.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_size,
+                            validation_data=(x_validation, y_validation))
+
+        plt.title('Loss')
+        plt.plot(history.history['loss'], label='train')
+        plt.plot(history.history['val_loss'], label='test')
+        plt.legend()
+        plt.show()
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from BasicLstmModel import BasicLstmModel
-
+from Model.LSTM_s import LSTM_s
 
 def main():
     print("Hello World!")
-    mymodel = BasicLstmModel(60,60)
-    mymodel.train_model()
+    #mymodel = BasicLstmModel(60,60)
+    #mymodel.train_model()
+    lstm = LSTM_s()
+    lstm.train_model()
 
 if __name__ == "__main__":
     main()
