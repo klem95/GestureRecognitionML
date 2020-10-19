e224d40ddc474275fa7b9dca82033617c9c55f47
Nikolaj Schlüter <nikolaj@bitkompagniet.dk>
2020-10-19 11:14:33 +0200

    Add untested prediction

--- Model/CNN_n_LSTM.py
+++ Model/CNN_n_LSTM.py
@@ -1,5 +1,5 @@
 import numpy as np
-from keras.models import Sequential
+from keras.models import Sequential, model_from_json
 from keras.optimizers import Adam
 from keras.layers import LSTM, Dense, Flatten, Embedding, Dropout, Conv2D, MaxPooling2D, TimeDistributed, Conv3D, MaxPooling3D
 from keras.optimizers import schedules
@@ -15,7 +15,7 @@ from numpy import load, save
 class CNN_n_LSTM:
 
     def __init__(self, lr, bs, e, split, f):
-        self.batch_size = 2
+        self.batch_size = 20 if bs is None else bs
         self.learning_rate = 0.01 if lr is None else lr
         self.epochs = 400 if e is None else e
         self.validationDataEvery = 5 if split is None else split
@@ -61,42 +61,46 @@ class CNN_n_LSTM:
         print('saving data to buffer')
         save('numpy-buffers/' + self.dataPath + '-npBuffer.npy', npObject)
 
-
-    def preprocess(self):
-        all_files = glob2.glob(self.dataPath + "/*.csv")
-        i = 0
+    def format(self, chunk):
         largestFrameCount = self.biggestDocLength()
         print('largestFrameCount')
         print(largestFrameCount)
 
+        frames = []
+        frame_count = 0
+        firstLine = True
+        for frame in chunk:
+            if firstLine:
+                firstLine = False
+                continue
+            coords = []  # x, y, z
+            for col in range(0, len(frame[:-1])):
+                if (col % 9 == 0 or col % 9 == 1 or col % 9 == 2):
+                    coords.append(np.asarray([frame[col]]))
+
+            joints = np.asarray(coords).reshape(-1, 3)  # produces 32 * 3
+            frames.append(np.asarray(joints).astype(float))
+            frame_count += 1
+
+        transposed = np.transpose(np.asarray(frames), (1, 0, 2))
+        print('(t, j, coords)')
+        transposed = transposed.reshape((transposed.shape[0], transposed.shape[1], transposed.shape[2], 1))
+
+        result = np.zeros((transposed.shape[0], largestFrameCount, transposed.shape[2], transposed.shape[3]))
+        result[:transposed.shape[0], :transposed.shape[1], : transposed.shape[2], :transposed.shape[3]] = transposed
+        print(np.asarray(result).shape)
+
+
+    def preprocess(self):
+        all_files = glob2.glob(self.dataPath + "/*.csv")
+        i = 0
+
         for filename in sorted(all_files):
             with open(filename, newline='') as csvfile:
                 print('loading: ' + filename)
-                firstLine = True
                 dataScanner = csv.reader(csvfile, delimiter=';', quotechar='|')
-                frames = []
-                frame_count = 0
-                for frame in dataScanner:
-                    if firstLine:
-                        firstLine = False
-                        continue
-                    coords = [] # x, y, z
-                    for col in range(0, len(frame[:-1])):
-                        if (col % 9 == 0 or col % 9 == 1 or col % 9 == 2):
-                            coords.append(np.asarray( [frame[col]] ))
 
-                    joints = np.asarray(coords).reshape(-1, 3) # produces 32 * 3
-                    frames.append(np.asarray(joints).astype(float))
-                    frame_count += 1
-
-                transposed = np.transpose(np.asarray(frames), (1, 0, 2))
-                print('(t, j, coords)')
-                transposed = transposed.reshape((transposed.shape[0], transposed.shape[1], transposed.shape[2], 1))
-
-                result = np.zeros((transposed.shape[0], largestFrameCount, transposed.shape[2], transposed.shape[3]))
-                result[:transposed.shape[0], :transposed.shape[1], : transposed.shape[2], :transposed.shape[3]] = transposed
-                print(np.asarray(result).shape)
-
+                result = self.format(dataScanner)
 
                 if i % self.validationDataEvery == 0:
                     self.validation_dataset.append(result)
@@ -117,6 +121,7 @@ class CNN_n_LSTM:
         print(np.asarray(self.onehotTrainLabels).shape)
 
 
+
     def encode_labels(self, file_names):
         mappedFileNames = []
         for filename in file_names:
@@ -127,6 +132,8 @@ class CNN_n_LSTM:
         onehot_encoded = self.oneHot_encoder.fit_transform(integer_encoded)
 
         return onehot_encoded
+
+
 
     def train_model(self):
         print('CNN Model')
@@ -139,13 +146,13 @@ class CNN_n_LSTM:
         bufferedNumpy = self.loadFromBuffer()
 
         if(bufferedNumpy != False):
-            print('Preprocessing files')
             x_train = bufferedNumpy[0]
             y_train = bufferedNumpy[1]
             x_validation = bufferedNumpy[2]
             y_validation = bufferedNumpy[3]
         else:
             self.preprocess()
+            print('Preprocessing files')
             x_train = np.asarray(self.train_dataset)
             y_train = np.asarray(self.onehotTrainLabels)
             x_validation = np.asarray(self.validation_dataset)
@@ -164,7 +171,6 @@ class CNN_n_LSTM:
         self.label_size = y_train.shape[1]
 
 
-        mcp_save = ModelCheckpoint('.mdl_wts.hdf5', save_best_only=True, monitor='val_loss', mode='min')
 
         lr_schedule = schedules.ExponentialDecay(
             initial_learning_rate=1e-2,
@@ -198,15 +204,43 @@ class CNN_n_LSTM:
 
         model.summary()
 
+        mcp_save = ModelCheckpoint('.mdl_wts.hdf5', save_best_only=True, monitor='val_loss', mode='min')
         history = model.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_size,
                             validation_data=(x_validation, y_validation), callbacks=[mcp_save])
         print(history.history.keys())
-
-        plt.title('Loss')
         print(mcp_save.best)
         plt.plot(history.history['accuracy'], label='train')
         plt.plot(history.history['val_accuracy'], label='validation')
         plt.legend()
         plt.show()
 
-        model.save(self.trained_model_path, 'Lstm_s')
+        self.saveModel(model)
+        #model.save(self.trained_model_path, 'Lstm_s')
+
+
+    def saveModel(self, model):
+        model_json = model.to_json()
+        with open("saved-models/model.json", "w") as json_file:
+            json_file.write(model_json)
+        # serialize weights to HDF5
+        model.save_weights("saved-models/model.h5")
+        print("Saved model to disk")
+
+    def loadModel(self):
+        json_file = open('saved-models/model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        # load weights into new model
+        loaded_model.load_weights("saved-models/model.h5")
+        print("Loaded model from disk")
+        return loaded_model
+
+
+    def predict(self, data):
+        formattedData = self.format(data)
+        loaded_model = self.loadModel()
+        loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+        score = loaded_model.predict(formattedData, verbose=0)
+        print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1] * 100))
+        return None
