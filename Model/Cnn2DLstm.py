d388b90b9ad93fab38c78e16d0499e2cf8a8b513
klem95 <enxbox@hotmail.com>
2020-11-30 11:03:58 +0100

    new model

--- Model/Cnn2DLstm.py
+++ Model/Cnn2DLstm.py
@@ -3,11 +3,13 @@ from numpy import load, save, genfromtxt
 import glob2
 from keras.optimizers import Adam
 from keras.layers import LSTM, Dense, Flatten, Embedding, Dropout, Conv2D, MaxPooling2D, TimeDistributed, Conv2D, \
-    Permute, Reshape, SpatialDropout2D
+    Permute, Reshape, SpatialDropout2D, Input, concatenate
 from keras.optimizers import schedules
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from keras.callbacks import ModelCheckpoint
+from keras.models import Model
+
 from keras.models import Sequential, model_from_json
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
@@ -32,6 +34,14 @@ class cnn2dlstm:
         self.validationFiles = []
         self.feature_size = 32 * 3
         self.modelType = 'cnn2dlstm'
+        self.labels = []
+
+        self.trunk_joint_count = 3
+        self.upper_region_joint_count = 8
+        self.lower_region_joint_count = 6
+        self.head_region = 10
+        self.full_body = 32
+
 
         if _loadModel:
             self.model = loadModel(self.path, self.modelType)
@@ -42,7 +52,56 @@ class cnn2dlstm:
         data = np.asarray(data)
         self.largest_region = self.get_largest_region_size()
         first_row = True
+
+        region_segmented_data = []
+        for region in range (0, len(Skeleton.region_look_up)):
+            region_segmented_data.append([None] * (len(Skeleton.region_look_up[region])*3))
+            for none_element in range(0,  len(region_segmented_data[-1])):
+                region_segmented_data[-1][none_element] =  []
+
+            print("Baseline region segmented data shape: ", len(region_segmented_data[region]))
+
+
+
+        for frame in data:
+            if first_row:
+                first_row = False
+                continue
+
+            col_count = 0
+            for col in range(0, len(frame[:-1])):
+                if col % 9 == 0 or col % 9 == 1 or col % 9 == 2:
+                    coord_index = col % 9
+                    for region in Skeleton.region_look_up:
+                        joint_index = 0
+                        for joint in Skeleton.region_look_up[region]:
+                            if col_count == joint:
+                                region_segmented_data[region][coord_index + joint_index * 3].append(frame[col])
+                            joint_index += 1
+                if col % 9 == 2:
+                    col_count += 1
+
+
+        for region_data in range(0,len(region_segmented_data)):
+            region_segmented_data[region_data] = np.asarray(region_segmented_data[region_data])
+
+        print("Output Data: ", len(region_segmented_data[0][1]))
+
+        region_segmented_data = np.asarray(region_segmented_data)
+        print("Type: ", type(region_segmented_data))
+        print("Type: ", type(region_segmented_data[0]))
+        print("Type: ", type(region_segmented_data[0][0]))
+
+
+        #zero_padded_result[:time_steps.shape[1], : time_steps.shape[2]] = time_steps
+
+        '''
+
         time_steps = []
+        segmented_data = []
+        segmented_data.append([])
+        segmented_data[0].append([])
+
         for frame in data:
             if first_row:
                 first_row = False
@@ -53,12 +112,14 @@ class cnn2dlstm:
                 if col % 9 == 0 or col % 9 == 1 or col % 9 == 2:
                     channel_pr_joint = []
                     for region in Skeleton.region_look_up:
-                        exist_in_region = False  # CHANGE TO FALSE FOR REGIONS !!! ...
+                        region_data = []
+                        exist_in_region = False
                         for joint in Skeleton.region_look_up[region]:
                             if col_count == joint:
                                 exist_in_region = True
                         if exist_in_region:
                             channel_pr_joint.append(frame[col])
+                            segmented_data[0, col].append(frame[col])
                         else:
                             channel_pr_joint.append(0)
                     if col % 9 == 2:
@@ -66,16 +127,15 @@ class cnn2dlstm:
                     total_coordinate_set.append(channel_pr_joint)
             time_steps.append(total_coordinate_set)
         time_steps = np.asarray(time_steps)
+        print(segmented_data.shape)
         result = np.zeros((largest_frame_count, time_steps.shape[1], time_steps.shape[2]))
         result[:time_steps.shape[0], :time_steps.shape[1], : time_steps.shape[2]] = time_steps
-        # print("Zero padded result: ", result)
+        print("Zero padded result: ", result.shape)
+        '''
 
-        # print("Head Region:")
-        # for joint in Skeleton.region_look_up[0]:
-        #     print("Joint", joint, ": x=", time_steps[0][joint * 3 + 0], "y=", time_steps[0][joint * 3 + 1], "z=",
-        #          time_steps[0][joint * 3 + 2])
 
-        return result
+
+        return region_segmented_data
 
     # print(self.largest_region)
     # print("Result: ", zero_padded_result[0])
@@ -117,8 +177,7 @@ class cnn2dlstm:
         print('traning onehot shape:')
         print(np.asarray(self.onehotTrainLabels).shape)
         print(np.asarray(self.onehotTrainLabels)[0])
-        print(np.asarray(self.onehotTrainLabels)[1])
-        print(np.asarray(self.onehotTrainLabels)[2])
+
 
         print('validation_dataset shape')
         print(np.asarray(self.validation_dataset).shape)
@@ -133,58 +192,51 @@ class cnn2dlstm:
         x_validation = np.asarray(self.validation_dataset)
         y_validation = np.asarray(self.onehotValidationLabels)
 
-        label_size = self.onehotValidationLabels.shape[1]
+        self.labels = self.onehotValidationLabels.shape[1]
 
         print("Input shape (x_train): ", x_train.shape)
         print("Input shape (y_train): ", y_train.shape)
         print("Input shape (x_validation): ", x_validation.shape)
         print("Input shape (y_validation): ", y_validation.shape)
 
+        x_train =  np.transpose(x_train)
+        x_validation =  np.transpose(x_validation)
+        print("Transposed", x_train.shape)
+        print("Transposed", x_validation.shape)
+
+        print(x_validation)
+
 
        #self.visualize_sample(x_train, 11)
         # print("y_train sample: ", x_train[4][4])
         # print("y_validation sample: ", y_validation[0])
 
-        model = Sequential()
-        
-        
-        
-        model.add(
-            Conv2D(filters=20, kernel_size=(9, 9), strides=(1, 3), activation='tanh', data_format="channels_last",
-                   input_shape=(self.largest_frame_count, self.feature_size, len(Skeleton.region_look_up))))
-        # model.add(Conv2D(filters=64, kernel_size=(5, 5), activation='tanh'))
-        model.add(Conv2D(filters=50, kernel_size=(6, 6), strides=(1, 3), activation='tanh'))
-        model.add(Conv2D(filters=100, kernel_size=(3, 3), strides=(1, 3), activation='tanh'))
-        #model.add(Conv2D(filters=256, kernel_size=(3, 3), strides=(3, 1), activation='tanh'))
+        trunk_input = Input(shape=(self.largest_frame_count, self.trunk_joint_count * 3),  name="trunk")
+        upper_left_input = Input(shape=(self.largest_frame_count, self.upper_region_joint_count * 3), name="upper_left")
 
-        time_steps = model.output_shape[1]
-        model.add(Reshape((time_steps, -1)))
-        # model.add(Permute((2, 1), input_shape=(time_steps, -1)))
+        trunk_lstm_0 = LSTM(units=20, return_sequences=True, recurrent_dropout=0.2)(trunk_input)
+        upper_left_lstm_0 = LSTM(units=20, return_sequences=True, recurrent_dropout=0.2)(upper_left_input)
 
-        model.add(LSTM(units=100, input_shape=model.output_shape, return_sequences=True, recurrent_dropout=0.2))
-        model.add(LSTM(units=75, return_sequences=True, recurrent_dropout=0.3))
-        model.add(LSTM(units=50, return_sequences=True, recurrent_dropout=0.2))
-        model.add(LSTM(units=25, recurrent_dropout=0.2))
+        concat_layer = concatenate([trunk_lstm_0, upper_left_lstm_0])
+        final_lstm_layer = LSTM(units=20, return_sequences=True, recurrent_dropout=0.2)(concat_layer)
 
-        model.add(Dropout(0.2))
-        model.add(Dense(300))
-        model.add(Dropout(0.1))
-        model.add(Dense(200))
-        model.add(Dropout(0.1))
-        model.add(Dense(100))
-        model.add(Dropout(0.2))
+        flatten = Flatten()(final_lstm_layer)
+        output = Dense(self.labels, activation='softmax') (flatten)
 
-        model.add(Flatten())
+        model = Model(inputs=[trunk_input,upper_left_input ], outputs=output)
 
-        model.add(Dense(label_size, activation='softmax'))  # Classification
-        model.compile(loss='categorical_crossentropy', optimizer=Adam(),
+        model.compile(loss = 'categorical_crossentropy', optimizer=Adam(),
                       metrics=['accuracy'])
 
         model.summary()
         mcp_save = ModelCheckpoint(self.path + 'saved-models/bestWeights.h5', save_best_only=True, monitor='val_loss',
                                    mode='min')
-        history = model.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_size,
-                            validation_data=(x_validation, y_validation), callbacks=[mcp_save])
+        history = model.fit([x_train[Skeleton.FULL_BODY],
+                              x_train[Skeleton.UPPER_LEFT_REGION]],
+                                y = y_train, epochs=self.epochs, batch_size=self.batch_size,
+                            validation_data=([x_validation[Skeleton.FULL_BODY], x_validation[Skeleton.UPPER_LEFT_REGION]],y_validation), callbacks=[mcp_save])
+
+
 
         plt.plot(history.history['loss'], label='train')
         plt.plot(history.history['val_loss'], label='validation')
