91c71dad927c42504354ff8d6e65305ad052ac89
Nikolaj Schlüter <nikolaj@bitkompagniet.dk>
2020-12-13 20:41:27 +0100

    Remove unused models

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 #from BasicLstmModel import BasicLstmModel
-from Model.LSTM_s import LSTM_s
-from Model.Cnn2DLstm import cnn2dlstm
-from Model.CnnLstm import cnnlstm
 import argparse
 import numpy as np
-from Model.CnnLstm import cnnlstm
+
+from Model.LSTM_s import LSTM_s
+from Model.Cnn2DLstm import cnn2dlstm
 from Model.Conv1d import conv1d
 from Model.Conv2d import conv2d
 from Model.LstmConv1d import denseConv1d
 
 LSTM = "lstm"
-CNN = "cnn"
-CNNLSTM = 'cnnltsm'
 CONV1D = 'conv1d'
 CONV2D = 'conv2d'
 LSTMCNN1D = 'lstmcnn1d'
@@ -27,7 +24,7 @@ CNN2LSTM = 'cnn2dlstm'
 def main():
 
     parser = argparse.ArgumentParser(description="AI Model Specifications")
-    parser.add_argument("-m", metavar='m', type=str, default=CNNLSTM)
+    parser.add_argument("-m", metavar='m', type=str, default=CNN2LSTM)
     parser.add_argument("-lr", metavar='l', type=float, default=0.5)
     parser.add_argument("-bs", metavar='bs', type=int, default=400)
     parser.add_argument("-e", metavar='e', type=int, default=20)
@@ -39,16 +36,6 @@ def main():
     if args.m == LSTM:
         lstm_model = LSTM_s(args.lr, args.bs, args.e, args.s, args.f, args.loadModel)
         lstm_model.train_model()
-    elif args.m == CNNLSTM:
-        model = cnnlstm(args.lr, args.bs, args.e, args.s, args.f, args.loadModel)
-        if(args.loadModel == False):
-            model.train_model()
-        else:
-            columnSize = 120
-            data = np.zeros((30, 289))
-            print(data.shape)
-            prediction = model.predict(data, columnSize, True)
-            print(prediction)
     elif args.m == CONV1D:
         model = conv1d(args.lr, args.bs, args.e, args.s, args.f, args.loadModel)
         if(args.loadModel == False):
